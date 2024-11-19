2b279cd1bb8e323968101a9a8d22855227cf2e2c
sangeethkumarpg <pgsangeethkumar@gmai.com>
2024-11-19 14:36:49 +0530

    Admin homepage cards functionality backend

--- controllers/complaintController.js
+++ controllers/complaintController.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const complaint = require("../models/complaintSchema");
 const mailer = require("../utils/mailer");
+const complaints = require("../models/complaintSchema");
 
 exports.createComplaint = async (req, res) => {
   try {
@@ -10,9 +11,9 @@ exports.createComplaint = async (req, res) => {
       requesterName,
       complaintType,
       dangerLevel,
-      location:{
-        lat:location.lat,
-        lng:location.lng
+      location: {
+        lat: location.lat,
+        lng: location.lng,
       },
       description,
     });
@@ -28,3 +29,71 @@ exports.createComplaint = async (req, res) => {
     return res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
+exports.getComplaints = async (req, res) => {
+  try {
+    const complaints = await complaint.find();
+    return res.status(200).json(complaints);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json("Internal server error");
+  }
+};
+
+exports.getUniqueComplaintCountByType = async (req, res) => {
+  try {
+    complaints.aggregate([
+      {
+        $group: {
+          _id: "$complaintType",
+          count: { $sum: 1 },
+        },
+      },
+      {
+        $sort: { count: -1 },
+      },
+    ]).then((result) => {
+      return res.status(200).json(result);
+    }).catch((err) => {
+      console.log(err);
+      return res.status(500).json("Internal server error");
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json("Internal server error");
+  }
+};
+
+exports.getUniqueDangerLevelCount = async(req,res)=>{
+  try {
+    complaints.aggregate([
+      {
+        $group: {
+          _id: "$dangerLevel",
+          count: { $sum: 1 },
+        },
+      },
+      {
+        $sort: { count: -1 },
+      }
+    ]).then((result) => {
+      return res.status(200).json(result);
+    }).catch((err)=>{
+      console.log(err);
+      return res.status(400).json("Unable to fetch data");
+    })
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json("Internal server error");
+  }
+}
+
+exports.getLatestComplaintLocations = async (req,res)=>{
+  try {
+    const latestLocations = await complaint.find({},{location:1, _id:0}).sort({createdData:-1}).limit(50);
+    return res.status(200).json(latestLocations);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json("Internal server error");
+  }
+}
--- routers/complaintRouter.js
+++ routers/complaintRouter.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const complaintController = require('../controllers/complaintController');
+const jwtMiddleWare = require('../middlewares/jwtMiddleWare');
 
 router.post('/new-complaint', complaintController.createComplaint);
+router.get('/get-all-complaints', jwtMiddleWare, complaintController.getComplaints);
+router.get('/get-complaint-count-by-type', jwtMiddleWare, complaintController.getUniqueComplaintCountByType);
+router.get('/get-complaint-count-by-danger-level', jwtMiddleWare, complaintController.getUniqueDangerLevelCount);
+router.get('/get-latest-complaint-locations', jwtMiddleWare, complaintController.getLatestComplaintLocations);
 
 module.exports = router;
